6110060d53f21833a2d2bf1d8ea97fd615a98c0f
Павел Нечкин <nechkinpa@gmail.com>
2022-06-17 19:58:03 +0400

    skeleton_v1

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import (
+    CategorySerializer,
+    GenreSerializer,
+    TitleSerializer,
+)
+from reviews.models import (
+    Category,
+    Genre,
+    Title,
+)
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    gueryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    gueryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    gueryset = Title.objects.all()
+    serializer_class = TitleSerializer
